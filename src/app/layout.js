bb875b3fdf145dacdea86afc20e8ed5cffc32340
pakhlavonovv <abdullokhpakhlavonov@gmail.com>
2025-06-21 15:27:48 +0500

    commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,14 +20,27 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
-          integrity="sha512-..."
-          crossOrigin="anonymous"
-          referrerPolicy="no-referrer"
-        />
-      </head>
+  <meta charSet="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="description" content="Frontend yo‘nalishini 8 oy ichida biz bilan o'rganing!" />
+  <meta name="keywords" content="UzDev, frontend kurs, o'zbekcha dasturlash, web development, HTML, CSS, Sass, Bootstrap, Tailwind css, Antd, Ant design, Mui, Material ui, JavaScript, js, React, Next" />
+  <meta name="author" content="UzDev Team" />
+  <meta property="og:title" content="UzDev - Frontend yo‘nalishida o‘zbekcha onlayn ta'lim" />
+  <meta property="og:description" content="Frontend yo‘nalishini 8 oy ichida biz bilan o'rganing!" />
+  <meta property="og:type" content="website" />
+  <meta name="twitter:title" content="UzDev - Frontend yo‘nalishida o‘zbekcha onlayn ta'lim" />
+  <meta name="twitter:description" content="Frontend yo‘nalishini 8 oy ichida biz bilan o'rganing!" />
+  <meta name="twitter:image" content="https://media.istockphoto.com/id/1356364287/photo/close-up-focus-on-persons-hands-typing-on-the-desktop-computer-backlit-keyboard-screens-show.jpg?s=612x612&w=0&k=20&c=ijjq-DLNxIaPuGvIX8k06IZxMAjGpyJeboaV_byCX9k=" />
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
+    integrity="sha512-..."
+    crossOrigin="anonymous"
+    referrerPolicy="no-referrer"
+  />
+  <title>UzDev - Frontend yo‘nalishida o‘zbekcha onlayn ta'lim</title>
+</head>
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
